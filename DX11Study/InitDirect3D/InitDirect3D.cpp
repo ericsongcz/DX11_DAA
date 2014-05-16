79ca1d88b77f5d1b8dca5db9b511c0a1a11a4622
erosnick <erosnick@gmail.com>
2014-05-16 17:12:54 +0800

    Init D3D.

--- DX11Study/InitDirect3D/InitDirect3D.cpp
+++ DX11Study/InitDirect3D/InitDirect3D.cpp
@@ -3,6 +3,28 @@
 
 #include "stdafx.h"
 #include "InitDirect3D.h"
+#include <d3dx11.h>
+
+#if defined(DEBUG) | defined(_DEBUG) 
+#ifndef HR
+#define HR(x) \
+{\
+	HRESULT hr = (x); \
+if (FAILED(hr)) \
+{\
+	DXTrace(__FILE__, (DWORD)__LINE__, hr, L#x, true); \ }\
+}
+#endif
+#else
+#ifndef HR 
+#define HR(x) (x) 
+#endif
+#endif
+
+float RGB256(UINT c)
+{
+	return 1.0f / 256.0f * c;
+}
 
 #define MAX_LOADSTRING 100
 
@@ -11,11 +33,23 @@ HINSTANCE hInst;								// 当前实例
 TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
 TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
 
+// D3D11 stuffs.
+ID3D11Device* device = nullptr;
+ID3D11DeviceContext* deviceContext = nullptr;
+IDXGISwapChain* swapChain = nullptr;
+ID3D11RenderTargetView* renderTargetView = nullptr;
+ID3D11Texture2D* depthStencilBuffer = nullptr;
+ID3D11DepthStencilView* depthStencilView = nullptr;
+
+bool mAppPaused = false;
+
 // 此代码模块中包含的函数的前向声明: 
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+bool InitD3D(HWND hWnd);
+void DrawScene();
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -43,12 +77,26 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_INITDIRECT3D));
 
 	// 主消息循环: 
-	while (GetMessage(&msg, NULL, 0, 0))
+	ZeroMemory(&msg, sizeof (MSG));
+	while (msg.message != WM_QUIT)
 	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		// If there are Window messages then process them.
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+		}
+		// Otherwise, do animation/game stuff.
+		else
+		{
+			if (!mAppPaused)
+			{
+				DrawScene();
+			}
+			else
+			{
+				Sleep(100);
+			}
 		}
 	}
 
@@ -107,6 +155,11 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
       return FALSE;
    }
 
+   if (!InitD3D(hWnd))
+   {
+	   return FALSE;
+   }
+
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
@@ -131,6 +184,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	switch (message)
 	{
+	case WM_ACTIVATE: 
+	if (LOWORD(wParam) == WA_INACTIVE)
+	{
+		mAppPaused = true;
+	}
+	else
+	{
+		mAppPaused = false;
+	}
+	break;
+
 	case WM_COMMAND:
 		wmId    = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
@@ -179,4 +243,116 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return (INT_PTR)FALSE;
+}
+
+bool InitD3D(HWND hWnd)
+{
+	UINT createDeviceFlags = 0;
+
+#if defined(DEBUG) || defined(_DEBUG)
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0
+	};
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof (DXGI_SWAP_CHAIN_DESC));
+
+	swapChainDesc.BufferDesc.Width = 800;
+	swapChainDesc.BufferDesc.Height = 600;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.OutputWindow = hWnd;
+	swapChainDesc.Windowed = true;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapChainDesc.Flags = 0;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, featureLevels, 3, 
+											   D3D11_SDK_VERSION, &swapChainDesc, &swapChain, &device, nullptr, &deviceContext);
+
+	if (FAILED(hr))
+	{
+		MessageBox(hWnd, TEXT("D3D11CreateDeviceAndSwapChain failed!"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	ID3D11Texture2D* backBuffer = nullptr;
+	swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&backBuffer));
+
+	hr = device->CreateRenderTargetView(backBuffer, 0, &renderTargetView);
+
+	if (FAILED(hr))
+	{
+		MessageBox(hWnd, TEXT("CreateRenderTargetView failed!"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	backBuffer->Release();
+
+	D3D11_TEXTURE2D_DESC depthStencilDesc;
+	ZeroMemory(&depthStencilDesc, sizeof (D3D11_TEXTURE2D_DESC));
+
+	depthStencilDesc.Width = 800;
+	depthStencilDesc.Height = 600;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.ArraySize = 1;
+	depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilDesc.SampleDesc.Count = 1;
+	depthStencilDesc.SampleDesc.Quality = 0;
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilDesc.CPUAccessFlags = 0;
+	depthStencilDesc.MiscFlags = 0;
+	
+	hr = device->CreateTexture2D(&depthStencilDesc, 0, &depthStencilBuffer);
+
+	if (FAILED(hr))
+	{
+		MessageBox(hWnd, TEXT("CreateTexture2D failed!"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	hr = device->CreateDepthStencilView(depthStencilBuffer, 0, &depthStencilView);
+
+	if (FAILED(hr))
+	{
+		MessageBox(hWnd, TEXT("CreateDepthStencilView failed!"), TEXT("Error"), MB_OK);
+		return false;
+	}
+
+	deviceContext->OMSetRenderTargets(1, &renderTargetView, depthStencilView);
+
+	D3D11_VIEWPORT viewport;
+	viewport.Width = 800.0f;
+	viewport.Height = 600.0f;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	viewport.TopLeftX = 0.0f;
+	viewport.TopLeftY = 0.0f;
+
+	deviceContext->RSSetViewports(1, &viewport);
+
+	return true;
+}
+
+void DrawScene()
+{
+	float clearColor[] = { RGB256(100), RGB256(149), RGB256(237) };
+
+	deviceContext->ClearRenderTargetView(renderTargetView, clearColor);
+	deviceContext->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	swapChain->Present(0, 0);
 }
