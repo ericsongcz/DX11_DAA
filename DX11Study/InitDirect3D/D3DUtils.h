e5d5d21bac8d0770867939590dcd7d4390e53b8a
erosnick <erosnick@gmail.com>
2014-05-22 16:25:52 +0800

    Add Color constants to D3DUtils.h.

--- DX11Study/InitDirect3D/D3DUtils.h
+++ DX11Study/InitDirect3D/D3DUtils.h
@@ -21,6 +21,20 @@
 #endif
 #endif 
 
+// Colors namespace defined in d3dUtil.h.
+//
+// #define XMGLOBALCONST extern CONST __declspec(selectany)
+// 1. extern so there is only one copy of the variable, and not a // separate private copy in each .obj.
+// 2. __declspec(selectany) so that the compiler does not complain // about multiple definitions in a .cpp file (it can pick anyone // and discard the rest because they are constant--all the same). namespace Colors
+XMGLOBALCONST XMVECTORF32 White = { 1.0f, 1.0f, 1.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Black = { 0.0f, 0.0f, 0.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Red = { 1.0f, 0.0f, 0.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Green = { 0.0f, 1.0f, 0.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Blue = { 0.0f, 0.0f, 1.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Yellow = { 1.0f, 1.0f, 0.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Cyan = { 0.0f, 1.0f, 1.0f, 1.0f }; 
+XMGLOBALCONST XMVECTORF32 Magenta = { 1.0f, 0.0f, 1.0f, 1.0f };
+
 float RGB256(UINT c);
 
 template<typename T>
