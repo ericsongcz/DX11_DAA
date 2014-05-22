e6e5c851e495b6c492a4a3a3e97819b44ae2c7c8
erosnick <erosnick@gmail.com>
2014-05-22 16:19:38 +0800

    Add Geometry class.

---
+++ DX11Study/InitDirect3D/Geometry.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "Geomety.h"
+
+bool Geometry::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext)
+{
+	mDevice = device;
+	mDeviceContext = deviceContext;
+
+
+}
---
+++ DX11Study/InitDirect3D/Geomety.h
@@ -0,0 +1,17 @@
+#include <d3dx11.h>
+#include <DirectXMath.h>
+
+class Geometry
+{
+public:
+	bool Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext);
+	void renderBuffer();
+private:
+	ID3D11Device* mDevice;
+	ID3D11DeviceContext* mDeviceContext;
+	ID3D11Buffer* mVertexBuffer;
+	ID3D11Buffer* mIndexBuffer;
+	ID3D11InputLayout* mInputLayout;
+	DWORD mVerticesCount;
+	DWORD mIndicesCount;
+};
